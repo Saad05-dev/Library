02ac70f2c945bc5512043e14a865806eb8452558
Saad05-dev <bougrasaad@gmail.com>
2025-11-17 16:11:18 +0100

    Adding display function plus some HTML and CSS

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const myLibrary = [];
 
+//Creating a book
 function Book(title,author,pages,read){
 
     this.id = crypto.randomUUID();
@@ -20,12 +21,39 @@ const theHobbit = new Book('The Hobbit','J.R.R. Tolkien',295,false);
 
 console.log(theHobbit.info());
 
+//Adding book to library
 function addBookToLibrary(title,author,pages,read)
 {
     myLibrary.push(new Book(title,author,pages,read));    
 }
+
 //test addBooktoLibrary function
 addBookToLibrary('The Hobbit','J.R.R. Tolkien',295,false);
 console.log("---");
 console.log(myLibrary[0]); 
 console.log(myLibrary[0].info());
+addBookToLibrary('Dune', 'Frank Herbert', 412, true);
+addBookToLibrary('A Wizard of Earthsea', 'Ursula K. Le Guin', 205, true);
+addBookToLibrary('Foundation', 'Isaac Asimov', 255, false);
+
+const bookContainer = document.getElementById('book-container');
+
+//display books 
+function displayBooks()
+{
+    bookContainer.innerHTML = ``;
+
+    myLibrary.forEach(book => {
+            const card = document.createElement('div');
+            card.classList.add('book-card');
+            card.innerHTML = `
+            <h3>${book.title}</h3>
+            <p class="author">by ${book.author}</p>
+            <p class="details">${book.pages} pages</p>
+            <p class="details">Status: ${book.read ? 'Read' : 'Not Read Yet'}</p>
+            `;
+            bookContainer.appendChild(card);
+    })  
+}
+displayBooks();
+
