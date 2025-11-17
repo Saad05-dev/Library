c2f1336d747820ece620ad4d2286fe48544262f0
Saad05-dev <bougrasaad@gmail.com>
2025-11-17 15:39:44 +0100

    Adding a function that adds books to library

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
+const myLibrary = [];
+
 function Book(title,author,pages,read){
 
+    this.id = crypto.randomUUID();
     this.title = title;
     this.author = author;
     this.pages = pages;
@@ -8,11 +11,21 @@ function Book(title,author,pages,read){
     this.info = function()
     {
         const readStatus = this.read ? "read." : "not read yet.";
-        return `${this.title} by ${this.author}, ${this.pages} pages, ${readStatus}.`;
+        return `${this.title} by ${this.author}, ${this.pages} pages, ${readStatus}`;
     }
 }
 
-
+//test Book constructor
 const theHobbit = new Book('The Hobbit','J.R.R. Tolkien',295,false);
 
 console.log(theHobbit.info());
+
+function addBookToLibrary(title,author,pages,read)
+{
+    myLibrary.push(new Book(title,author,pages,read));    
+}
+//test addBooktoLibrary function
+addBookToLibrary('The Hobbit','J.R.R. Tolkien',295,false);
+console.log("---");
+console.log(myLibrary[0]); 
+console.log(myLibrary[0].info());
